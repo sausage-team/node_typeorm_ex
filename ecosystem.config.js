a28a547cbf5d022738a68240380f5756e8c9cef1
tangdaoyuan <1197633750@qq.com>
2019-06-20 15:44:27 +0800

    ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,6 @@ module.exports = {
   apps : [{
     name: 'Node_TypeOrm_Ts',
     script: 'ts-node',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'src/index.ts',
     instances: 1,
     autorestart: true,
@@ -19,12 +17,17 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'user' : 'node',
+      'host' : '212.83.163.1',
+      'ref'  : 'origin/master',
+      'repo' : 'git@github.com:repo.git',
+      'path' : '/var/www/production',
+      'ssh_options' : "StrictHostKeyChecking=no",
+      'pre-deploy': 'git fetch --all',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'env' : {
+        "NODE_ENV": "production"
+      }
     }
   }
 };
